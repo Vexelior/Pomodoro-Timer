7c4858b80087f53297e217938b04df94bd51cd82
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2022-06-28 14:09:08 -0700

    more tweaks and fixes for buttons

--- js/main.js
+++ js/main.js
@@ -1,29 +1,49 @@
-const timerArea = document.getElementById('timer');
-const twentyFiveTimer = document.getElementById('25timer');
-const fiveTimer = document.getElementById('5timer');
-const fifteenTimer = document.getElementById('15timer');
+let timerArea = document.getElementById('timer');
+let twentyFiveTimer = document.getElementById('25timer');
+let fiveTimer = document.getElementById('5timer');
+let fifteenTimer = document.getElementById('15timer');
 
+// Set timer area to zero initially
 timerArea.innerHTML = '00:00';
 
-function setTimerTo5Minutes() {
+function SetTimerTo5Minutes() {
     timerArea.innerHTML = '05:00';
 }
 
-function setTimerTo15Minutes() {
+function SetTimerTo15Minutes() {
     timerArea.innerHTML = '15:00';
 }
 
-function setTimerTo25Minutes() {
+function SetTimerTo25Minutes() {
     timerArea.innerHTML = '25:00';
 }
 
-function startTimer() {
+function DisableTimerButtons() {
+    twentyFiveTimer.disabled = true;
+    fiveTimer.disabled = true;
+    fifteenTimer.disabled = true;
+}
+
+function EnableTimerButtons() {
+    twentyFiveTimer.disabled = false;
+    fiveTimer.disabled = false;
+    fifteenTimer.disabled = false;
+}
+
+function StartTimer() {
+    DisableTimerButtons();
     let timer = timerArea.innerHTML.split(':');
     let minutes = parseInt(timer[0]);
     let seconds = parseInt(timer[1]);
     let totalSeconds = minutes * 60 + seconds;
     let audio = new Audio('media/alarm.mp3');
     let resetButton = document.getElementById('reset');
+
+    if (timerArea.innerHTML == '00:00') {
+        alert('Please select a timer.');
+        EnableTimerButtons();
+        return;
+    }
 
     let interval = setInterval(function () {
         totalSeconds--;
@@ -39,7 +59,8 @@ function startTimer() {
 
         if (totalSeconds === -1) {
             audio.play();
-            timerArea.innerHTML = 'Time is up!';
+            alert('Time is up!');
+            EnableTimerButtons()
             clearInterval(interval);
             resetTimer();
         }
@@ -48,6 +69,7 @@ function startTimer() {
     function resetTimer() {
         clearInterval(interval);
         timerArea.innerHTML = '00:00';
+        EnableTimerButtons()
     }
     resetButton.addEventListener('click', resetTimer);
 }
